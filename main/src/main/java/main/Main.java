ab49b92e98794986e4ba6a66e4a75a057998cfdb
Elderion64 <antoine.barachet@viacesi.fr>
2017-06-14 14:15:54 +0200

    test de Eld

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -27,6 +27,7 @@ public abstract class Main {
             controller.start();
         } catch (final SQLException exception) {
             exception.printStackTrace();
+            // hello le projet horrible
         }
     }
 
